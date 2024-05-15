0ac80b1d0b5ba425292a7d747c18e9ed86c3b5b6
readyshen <readyshen@gmail.com>
2024-05-15 22:59:32 +0800

    错误出现的原因是alien.py中的settings对象与alien_invasion.py中不一样

--- alien.py
+++ alien.py
@@ -8,7 +8,7 @@ class Alien(Sprite):
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
-        self.settings = Settings()
+        self.settings = ai_game.settings
         self.imageFile = 'images/UFO.png'
         self.speed = self.settings.alien_speed
 
