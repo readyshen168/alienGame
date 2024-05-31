26a1b6c2bf4bde79b1d443ef4a7a370e2a1e5f6b
readyshen <readyshen@gmail.com>
2024-05-31 22:32:57 +0800

    删除一些多余的代码

--- alien_invasion.py
+++ alien_invasion.py
@@ -39,16 +39,10 @@ class AlienInvasion:
         # 设置游戏窗口标题
         pygame.display.set_caption("Alien Invasion")
 
-        # 创建外星战队
-        # self.aliens = pygame.sprite.Group()
-        # self._create_fleet()
-
         '''创建外星舰队队长，负责管理外星舰队的各类行为'''
         self.alien_captain = Alien(self)
         # 创建外星战队
         self.alien_captain.create_fleet()
-        # 外星战队精灵组
-        # self.aliens = self.alien_captain.aliens
 
         # 创建飞船
         self.ship = Ship(self)
@@ -121,28 +115,6 @@ class AlienInvasion:
             # 重建外星人舰队和我方飞船及还原游戏难度、隐藏光标
             self._recreate_fleet_ship()
 
-    # 创建外星舰队
-    '''
-    def _create_fleet(self):
-
-        # 飞船之间的间距取飞船的长宽值
-        alien_width = self.alien_captain.rect.width
-        alien_height = self.alien_captain.rect.height
-        # 飞船当前的坐标
-        current_x, current_y = alien_width, alien_height
-        # 开始循环，条件：当前坐标离屏幕边沿保持一定距离
-        while current_y < (self.settings.screen_height - 30 * alien_height):
-            while current_x < (self.settings.screen_width - 10 * alien_width):
-                # 根据x坐标生成新的外星飞船
-                self._create_alien(current_x, current_y)
-                # 当前x坐标加上两倍的间距
-                current_x += 2 * alien_width
-            # 新的一行需要重置x坐标
-            current_x = alien_width
-            # 当前y坐标加上2倍的间距
-            current_y += 2 * alien_height
-    '''
-
     # 提升游戏难度
     def _upgrade_difficulty(self):
         # 提升速度
@@ -164,71 +136,6 @@ class AlienInvasion:
         pygame.mouse.set_visible(False)
         # 还原游戏难度设置 initialize_dynamic_settings()
         self.settings.initialize_difficulty_settings()
-
-    # 根据坐标生成外星飞船并加入舰队
-    '''
-    def _create_alien(self, x, y):
-        # 新建一艘飞船，设定其X坐标
-        new_alien = Alien(self)
-        new_alien.x = x
-        new_alien.y = y
-        new_alien.rect.x = x
-        new_alien.rect.y = y
-        # 将飞船加入舰队
-        self.aliens.add(new_alien)
-        '''
-
-    # 在有外星飞船到达屏幕边缘时采取相应的措施：
-    # 外星舰队的飞行规则：水平持续飞行，碰到屏幕边缘则向下移动并改变水平飞行方向
-    '''
-    def _check_fleet_edges(self):
-        for alien in self.aliens.sprites():
-            if alien.check_edges():
-                self._change_fleet_direction()
-                break
-    '''
-
-    # 将外星舰队向下移动，并改变水平移动方向
-    ''' 
-    def _change_fleet_direction(self):
-        # 当外星舰队中某只飞船到达屏幕边缘，则整体舰队向下移动
-        for alien in self.aliens.sprites():
-            alien.rect.y += self.settings.fleet_drop_speed
-            # alien.rect.y = alien.y
-        self.settings.fleet_direction *= -1
-     '''
-
-    # 检测是否有外星人到达屏幕下边缘
-    '''
-    def _check_aliens_bottom(self):
-        # 像飞船被撞到一样处理_ship_hit
-        for alien in self.alien_captain.aliens.sprites():
-            if alien.rect.bottom >= self.screen.get_rect().bottom:
-                self._ship_hit()
-                break
-    '''
-
-    # 处理飞船与外星人相撞的方法:
-    '''
-    def _ship_hit(self):
-        if self.game_stats.ships_left > 0:
-            # 将game_stats中的ships_left减1
-            self.game_stats.ships_left -= 1
-            # 清空外星人
-            # self.aliens.empty()
-
-            # 创建新的外星人舰队（会清空子弹）），并将飞船重置于屏幕底部中央ship.center_ship()
-            self._recreate_fleet_ship()
-            # self._recreate_fleet()
-            # self.ship.center_ship()
-
-            # 暂停
-            # pygame.time.delay(500)
-        else:
-            self.game_active = False
-            # 显示光标
-            pygame.mouse.set_visible(True)
-    '''
 
     # 结束本轮游戏，并重新生成外星舰队、我方飞船、play按钮的方法，调用该方法时，self.game_active为True
     def _end_round(self):
@@ -255,18 +162,6 @@ class AlienInvasion:
 
     # 更新外星舰队状态
     def _update_aliens(self):
-        # 外星飞船绘制
-        # self.alien.draw_alien()
-
-        # 外星队长检查舰队是否碰到边缘，若碰到则下移且改变水平移动方向
-        """
-        self.alien_captain.check_fleet_edges()
-        # 更新舰队状态
-        self.alien_captain.aliens.update()
-        # 外星舰队绘制
-        self.alien_captain.aliens.draw(self.screen)
-        """
-
         # 由外星队长更新外星舰队的位置
         self.alien_captain.update_aliens()
 
